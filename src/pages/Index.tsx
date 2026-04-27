4656d4a31796fadeaf2c2b0eada1b566511ab0c0
benchav <joshua44benja@gmail.com>
2026-04-27 09:58:55 -0600

    new proyect

---
+++ src/pages/Index.tsx
@@ -0,0 +1,17 @@
+const Index = () => {
+  return (
+    <iframe
+      src="/invitacion.html"
+      title="Invitación - Concurso Aplicaciones Móviles 2026 | UNAN-Carazo"
+      style={{
+        position: "fixed",
+        inset: 0,
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+};
+
+export default Index;
